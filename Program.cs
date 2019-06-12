91644e3ad6348673f5c5b9d884486cef76a3a6a0
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-06-12 09:12:12 -0400

    first

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace a_whole_new_world
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var Min = 1;
+      var Max = 100;
+      var Input = "";
+      Console.WriteLine("Pick a number between 1 and 100");
+      while (Input != "yes")
+      {
+        var Guess = ((Min + Max) / 2);
+        Console.WriteLine($"Is your number {Guess}?");
+        Input = Console.ReadLine();
+
+        if (Input == "no")
+        {
+          Console.WriteLine("higher or lower");
+          Input = Console.ReadLine();
+          if (Input == "higher")
+          {
+            Min = Guess;
+          }
+          else if (Input == "lower")
+          {
+            Max = Guess;
+          }
+        }
+      }
+
+      Console.WriteLine("Yay");
+
+    }
+  }
+}
